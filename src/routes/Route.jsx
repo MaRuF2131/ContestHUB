8d38801aac6bd28940337b4eaa71069018b05e0e
MaRuF2131 <marufahmmed916@gmail.com>
2025-12-12 20:45:57 +0600

    not found page added

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 
 
  export const Route=createBrowserRouter([
     {
       path:'/',
       element:<Home></Home>
+    },
+    {
+        path:'*',
+        element:<NotFound></NotFound>
     }
+
 ])
