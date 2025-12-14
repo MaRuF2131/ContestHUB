7c1eb70097b68e340172acd476514297173cd121
MaRuF2131 <marufahmmed916@gmail.com>
2025-12-14 23:30:22 +0600

    login setup

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,8 +1,12 @@
 import { motion } from "framer-motion";
+import {  useState } from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../../hook/UseAuth";
 
 
 export default function Navbar() {
+ const{user,loading,logout}=useAuth();
+ const [isDropdownOpen, setIsDropdownOpen] = useState(false);   
 return (
 <motion.nav
 initial={{ y: -30, opacity: 0 }}
@@ -19,6 +23,42 @@ ContestHub
 <Link to="/">Home</Link>
  <Link to="/all-contest">All Contests</Link>
 <Link to="/leaderboard">Leaderboard</Link> 
+</div>
+<div>
+{loading ? (
+<p className="text-black dark:text-white">Loading...</p>
+) : user ? (    
+    <div className="relative md:block hidden">
+        <img
+            src={user.photoURL || 'https://via.placeholder.com/40'}
+            alt="User Profile"
+            referrerPolicy='no-referrer'
+            className="w-10 h-10 rounded-full cursor-pointer"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        />
+        {isDropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg py-2 z-50">
+                <Link
+                    to="/dashboard"
+                    className="block px-4 py-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                    Dashboard
+                </Link>
+                <button type="button"
+                    onClick={() => logout()}
+                    className="w-full text-left block px-4 py-2 text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                   >
+                    Logout
+                </button>
+            </div>
+        )}
+    </div>
+) : (
+<Link
+to="/login"
+className="text-black dark:text-white font-medium"
+>   Login</Link>
+)}
 </div>
 </motion.nav>
 );
